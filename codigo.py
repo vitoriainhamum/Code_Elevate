c3af8da17f23b4ac2def9a43703e95686ef20600
vitoriainhamum <vitoriainhamum@hotmail.com>
2025-03-22 18:18:59 -0300

    Alteração para gravar tabela delta e ler os dados utilizando ela como origem

--- codigo.py
+++ codigo.py
@@ -1,8 +1,6 @@
-# Importando as bibliotecas necessárias
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, avg, min, max, sum, desc, regexp_extract
-from pyspark.sql.functions import to_date
-from pyspark.sql.functions import dayofweek
+from pyspark.sql.functions import col, count, avg, min, max, sum, desc, regexp_extract, to_date, dayofweek, date_format, when
+import matplotlib.pyplot as plt
 
 # Inicializando SparkSession
 spark = SparkSession.builder.appName("LogAnalysis").getOrCreate()
@@ -10,19 +8,19 @@ spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
 
 # Carregando os dados
 file_path = "/FileStore/tables/access_log.txt"
-logs_df = spark.read.text(file_path)
-
+delta_path = "/mnt/delta/logs_delta"  # Caminho para salvar a tabela Delta
 
 # Definição das expressões regulares para cada campo
 regex_patterns = {
-    "ip": r"^(\S+)", 
-    "timestamp": r"\[(.*?)\]",  
+    "ip": r"^(\S+)",  
+    "timestamp": r"\[(.*?)\]", 
     "method": r"\"(\S+)",  
     "endpoint": r"\"[^\"]* (\S+) HTTP",  
     "status": r" (\d{3}) ",  
-    "bytes": r" (\d+)$"  
+    "bytes": r" (\d+)$" 
 }
 
+logs_df = spark.read.text(file_path)
 
 # Aplicação das expressões para criar novas colunas
 logs_df = logs_df \
@@ -36,11 +34,15 @@ logs_df = logs_df \
 # Removendo a coluna original para manter o DataFrame limpo
 logs_df = logs_df.drop("value")
 
+# Gravando os dados processados como tabela Delta
+logs_df.write.format("delta").mode("overwrite").save(delta_path) #Salvando tabela delta
+df_delta = spark.read.format("delta").load(delta_path) #Lendo tabela delta
+
 #Identifique as 10 maiores origens de acesso (Client IP):
-logs_df.groupBy("ip").count() \
-       .orderBy(desc("count"))\
-       .limit(10) \
-       .show()
+df_delta.groupBy("ip").count() \
+        .orderBy(desc("count")) \
+        .limit(10) \
+        .show()
 
 # Criar um gráfico com os 10 IPs mais frequentes
 ip_counts = logs_df.groupBy("ip").count().orderBy(desc("count")).limit(10).toPandas()
@@ -53,22 +55,23 @@ plt.show()
 
 #Liste os 6 endpoints mais acessados (desconsiderando arquivos):
 # Filtrando endpoints sem extensão de arquivos
-filtered_df = logs_df.filter(~col("endpoint").rlike(r"\.\w+$"))
+filtered_df = df_delta.filter(~col("endpoint").rlike(r"\.\w+$"))  
 
 filtered_df.groupBy("endpoint").count() \
-           .orderBy(desc("count"))\
+           .orderBy(desc("count")) \
            .limit(6) \
            .show()
 
-#Quantidade de Client IPs distintos:
-logs_df.select("ip").distinct().count()
+#Quantidade de Client IPs distintos
+distinct_ips = df_delta.select("ip").distinct().count()
+print(f"Quantidade de IPs distintos: {distinct_ips}")
 
 #Quantos dias de dados estão representados no arquivo:
-logs_df.withColumn("date", to_date("timestamp", "dd/MMM/yyyy")) \
-       .select("date").distinct().count()
+df_delta.withColumn("date", to_date("timestamp", "dd/MMM/yyyy")) \
+        .select("date").distinct().count()
 
 #Análise dos volumes de dados (em bytes):
-logs_df.agg(
+df_delta.agg(
     sum("bytes").alias("total_volume"),
     max("bytes").alias("max_volume"),
     min("bytes").alias("min_volume"),
@@ -76,10 +79,8 @@ logs_df.agg(
 ).show()
 
 #Dia da semana com maior número de erros
-logs_df.filter(col("status").startswith("4")) \
+df_delta.filter(col("status").startswith("4")) \
         .withColumn("day_of_week", date_format(to_date("timestamp", "dd/MMM/yyyy"), "EEEE")) \
         .groupBy("day_of_week").count() \
         .orderBy(desc("count")) \
         .show()
-
-logs_df.write.format("delta").mode("overwrite").save("/mnt/delta/logs_delta")
